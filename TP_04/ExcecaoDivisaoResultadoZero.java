2b05a26e0a7109d8c4818f7e729f1c53f47c2d24
Eliane Nunes <elianu.nun.mar@gmail.com>
2021-06-27 23:21:21 -0300

    Compartilhamento inicial do projeto

---
+++ TP_04/ExcecaoDivisaoResultadoZero.java
@@ -0,0 +1,12 @@
+package TP_04;
+
+
+public class ExcecaoDivisaoResultadoZero extends Exception {
+    public ExcecaoDivisaoResultadoZero(String msg){
+        super(msg);
+    }
+    public ExcecaoDivisaoResultadoZero(String msg, Throwable causa){
+        super(msg,causa);
+    }
+    
+}
---
+++ TP_04/main.java
@@ -0,0 +1,19 @@
+package TP_04;
+
+
+public class main {
+    public static void main(String[] args)throws ExcecaoDivisaoResultadoZero{
+        int a[] = new int[10]; 
+        int b[] = new int[10]; 
+        for(int i = 0;i<10;i++){
+            a[i] = 0;
+            b[i] = i;
+        }
+        Array teste = new Array();
+        teste.setArrayA(a);
+        teste.setArrayB(b);
+        teste.CalculaDivisaoArrays();
+
+    
+}
+}
